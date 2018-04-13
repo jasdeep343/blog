42900c2a6d16c14318870ace50edcfac30d80024
jasdeep <singhjazz88@gmail.com>
2018-04-14 01:39:51 +0530

    index integration

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Abstract</title>
+    <meta name="description" content="">  
+	<meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/vendor.css">  
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+</head>
+
+<body id="top">
+<header class="short-header">   
+    <div class="row header-content">
+        <div class="logo">
+            <a href="index.html">Author</a>
+        </div>
+        <nav id="main-nav-wrap">
+            <ul class="main-navigation sf-menu">
+                <li class="current"><a href="index.html" title="">Home</a></li>									
+                <li class="has-children">
+                    <a href="category.html" title="">Categories</a>
+                    <ul class="sub-menu">
+                        <li><a href="category.html">Wordpress</a></li>
+                        <li><a href="category.html">HTML</a></li>
+                        <li><a href="category.html">Photography</a></li>
+                        <li><a href="category.html">UI</a></li>
+                        <li><a href="category.html">Mockups</a></li>
+                        <li><a href="category.html">Branding</a></li>
+                    </ul>
+                </li>
+                <li class="has-children">
+                    <a href="single-standard.html" title="">Blog</a>
+                    <ul class="sub-menu">
+                        <li><a href="single-video.html">Video Post</a></li>
+                        <li><a href="single-audio.html">Audio Post</a></li>
+                        <li><a href="single-gallery.html">Gallery Post</a></li>
+                        <li><a href="single-standard.html">Standard Post</a></li>
+                    </ul>
+                </li>
+                <li><a href="style-guide.html" title="">Styles</a></li>
+                <li><a href="about.html" title="">About</a></li>	
+                <li><a href="contact.html" title="">Contact</a></li>										
+            </ul>
+        </nav>  
+    </div>     
+</header>
+            @yield('content')
+
+
+<footer>
+    <div class="footer-bottom">
+        <div class="row">
+            <div class="col-twelve">
+                <div class="copyright">
+                    <span>© Copyright Abstract 2016</span> 
+                    <span>Design by <a href="http://www.styleshout.com/">styleshout</a></span>		         	
+                </div>
+                <div id="go-top">
+                    <a class="smoothscroll" title="Back to Top" href="#top"><i class="icon icon-arrow-up"></i></a>
+                </div>         
+            </div>
+        </div> 
+    </div>
+</footer>            
+</body>
+<script src="js/modernizr.js"></script>
+<script src="js/pace.min.js"></script>
+<script src="js/jquery-2.1.3.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/jquery.appear.js"></script>
+<script src="js/main.js"></script>
+</html>
